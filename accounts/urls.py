74137f39ce1f085f1cac938921da188f5d0735b1
Changhyun Lee <chang12@snu.ac.kr>
2017-07-27 01:30:02 +0900

    User 생성하는 view 추가

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from .views import token_create, user_create
+
+urlpatterns = [
+    url(r'^$', user_create)
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import User
 
 # Create your views here.
+
+
+@csrf_exempt
+def user_create(request):
+    assert request.method == "POST"
+    assert "email" in request.POST  # verification 로직은 사실 view 레이어가 아니라 serializer 레이어로 옮기는게 좋음!
+    assert "password" in request.POST
+
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+
+    user = User()
+    user.email = email
+    user.set_password(password)  # user.password = password 로 바로 assign 하면 raw password 가 그대로 저장되어버린다.
+    user.username = email  # Django 에서 username 컬럼이 UNIQUE constraint -> IntegrityError 를 피하기 위한 임시방편!
+    user.save()
+
+    data = {
+        "id": user.pk
+    }
+
+    return JsonResponse(data=data)
+
