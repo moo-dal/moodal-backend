3674d4004a423b9243a1963de0b303ef38f24be4
Changhyun Lee <chang12@snu.ac.kr>
2017-07-27 01:31:26 +0900

    jwt 생성하는 view 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from .views import token_create, user_create
 
 urlpatterns = [
-    url(r'^$', user_create)
+    url(r'^$', user_create),
+    url(r'^tokens/$', token_create)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
+import time
+
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import jwt
 
 from .models import User
+
+TOKEN_SECRET = "temp-secret"  # 배포 환경에서 config.py 등의 파일로 빼내고 VCS 에서 제거할 예정~
+TOKEN_EXPIRY_MS = 1000 * 3600 * 3  # 3 hours. 변환해주는 라이브러리가 있으면 좋겠다.
 
 # Create your views here.
 
@@ -28,3 +34,27 @@ def user_create(request):
 
     return JsonResponse(data=data)
 
+
+@csrf_exempt
+def token_create(request):
+    assert request.method == "POST"
+    assert "email" in request.POST  # verification 로직은 사실 view 레이어가 아니라 serializer 레이어로 옮기는게 좋음!
+    assert "password" in request.POST
+
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+
+    user = User.objects.get(email=email)
+    assert user.check_password(password)
+
+    payload = {
+        "email": email,
+        "id": user.pk,
+        "expiry": int(time.time() * 1000) + TOKEN_EXPIRY_MS
+    }
+    token = jwt.encode(payload=payload, key=TOKEN_SECRET).decode("utf-8")
+    data = {
+        "token": token
+    }
+
+    return JsonResponse(data=data)
