e53dd88a3b2c282e928b790a94b255d15a29a1f1
HyunyoungKim <ball4716@naver.com>
2017-08-18 23:39:41 +0900

    모델 작성

--- schedule/models.py
+++ schedule/models.py
@@ -3,12 +3,27 @@ from django.db import models
 from accounts.models import User
 # Create your models here.
 
+
+class Calendar(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Preference(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+
+
 class Schedule(models.Model):
     title = models.CharField(max_length=50)
     url = models.URLField()
     start_date = models.DateField()
     end_date = models.DateField()
-    is_shared = models.BooleanField()
-    is_public = models.BooleanField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    #category_name = models.ForeignKey()
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+
+
+class Mapping(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=True)
+
