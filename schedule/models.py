8bbffd351f1da525571432c527906761bb4e9640
Changhyun Lee <chang12@snu.ac.kr>
2017-08-21 23:28:55 +0900

    Preference 에 unique constraint 추가

--- schedule/models.py
+++ schedule/models.py
@@ -12,6 +12,9 @@ class Calendar(models.Model):
 
 
 class Preference(models.Model):
+    class Meta:
+        unique_together = (("user", "calendar"), )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
 
