516c9abe10070070f19bdca799ebc65f67f76520
Changhyun Lee <chang12@snu.ac.kr>
2017-08-21 23:50:43 +0900

    ScheduleSerializer 정리

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -5,10 +5,12 @@ from .models import Schedule
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    calendar_id = serializers.IntegerField()
+    user_id = serializers.IntegerField()
+
     class Meta:
         model = Schedule
-        fields = ("title", "url", "start_date", "end_date", "is_shared", "is_public", "user")
-        #categori_name추가해야함
+        fields = ("title", "url", "start_date", "end_date", "calendar_id", "user_id")
     '''
     모델 Serializer 를 상속하므로 상단처럼 하면 필드 자동 생성
     name = serializers.CharField(max_length=50)
@@ -19,20 +21,20 @@ class ScheduleSerializer(serializers.ModelSerializer):
     is_public = serializers.BooleanField()
     '''
     def create(self, validated_data):
-        user_id = self.context['request'].user.id
         schedule = Schedule()
         schedule.title = validated_data["title"]
         schedule.url = validated_data["url"]
         schedule.start_date = validated_data["start_date"]
         schedule.end_date = validated_data["end_date"]
-        schedule.is_shared = validated_data["is_shared"]
-        schedule.is_public = validated_data["is_public"]
-        schedule.user_id = user_id
-        schedule.save()
-        return schedule
 
-    def to_representation(self, instance):
-        result = {
-            "result": "success"
-        }
-        return result
+        user_id = self.context['request'].user.id
+        calendar_id = validated_data["calendar_id"]
+        user = User.objects.get(pk=user_id)
+        calendar = Calendar.objects.get(pk=calendar_id)
+
+        schedule.user = user
+        schedule.calendar = calendar
+
+        schedule.save()
+
+        return schedule
