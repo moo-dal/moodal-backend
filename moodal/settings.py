0d1341f763517aa723f1315d1b3b1bc54f48b8f1
Changhyun Lee <chang12@snu.ac.kr>
2017-08-21 23:38:29 +0900

    클라이언트를 위해 CORS 를 Allow-All 로 열어줌

--- moodal/settings.py
+++ moodal/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
+    'corsheaders',
     'accounts.apps.AccountsConfig',
     'schedule.apps.ScheduleConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,3 +142,5 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
