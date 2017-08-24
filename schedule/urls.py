204c597594d763c291c71d17ef7130499421cbb2
Max <max@vcnc.co.kr>
2017-08-25 00:06:28 +0900

    에러 수

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from .views import CalendarList, PreferenceCreate, ScheduleListCreate
+from .views import CalendarList, PreferenceListCreate, ScheduleListCreate
 
 urlpatterns = [
     url(r'^calendars$', CalendarList.as_view()),
-    url(r'^preferences$', PreferenceCreate.as_view()),
+    url(r'^preferences$', PreferenceListCreate.as_view()),
     url(r'^schedules$', ScheduleListCreate.as_view()),
 ]
